ca1dba0376120cbfc3f57e5bca847da2ec135b3f
Moataz Elmesmary <92026137+Moataz-Elmesmary@users.noreply.github.com>
2022-05-07 01:50:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app = Flask(__name__,static_url_path='', static_folder='./templates')
 @app.route('/')
 def home():
     try:
-        return render_template('./predict.html')
+        return render_template('./home.html')
     except:
         print("Oops!", sys.exc_info()[0], "occurred.")
 
